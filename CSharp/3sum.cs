cd4fc721f916a5b2e56056ac82eb7920632e590c
Hainan Zhao <Hainan.Zhao@sage.com>
2021-01-21 09:53:03 +0800

    Added more code

---
+++ CSharp/3sum.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreeSum
+{
+    public class Solution
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            var list = new List<IList<int>>();
+            if (nums.Length < 3)
+                return list;
+
+            Array.Sort(nums);
+
+            var len = nums.Length;
+            for (int i = 0; i < len - 2; i++)
+            {
+
+                var target = -nums[i];
+
+                //Skip the duplicate 1st int
+                if (i > 0 && nums[i - 1] == nums[i])
+                    continue;
+
+                int slow = i + 1, fast = len - 1;
+                while (slow < fast)
+                {
+                    if (nums[slow] + nums[fast] > target)
+                    {
+                        fast--;
+                    }
+                    else if (nums[slow] + nums[fast] < target)
+                    {
+                        slow++;
+                    }
+                    else
+                    {
+                        var li = new List<int>() { nums[i], nums[slow], nums[fast] };
+                        list.Add(li);
+                        slow++;
+
+                        //Skip the duplicate 2nd int
+                        while (slow < fast && nums[slow] == nums[slow - 1])
+                            slow++;
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ CSharp/reverse-bits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReverseBits
+{
+    public class Solution
+    {
+        public uint reverseBits(uint n)
+        {
+            uint result = 0;
+            for (var i = 0; i < 32; i++)
+            {
+                result <<= 1;
+                //Combine the bits in result and the last bit of n. Can also use the + operator.
+                result |= (n & 1);                
+                n >>= 1;
+            }
+            return result;
+        }
+    }
+}
