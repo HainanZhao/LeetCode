baad21265b2c6d33ccf2083b9ea482fbc7251583
Hainan Zhao <Hainan.Zhao@sage.com>
2021-01-21 14:00:54 +0800

    Update bit shift algo

--- CSharp/bitwise-and-of-numbers-range.cs
+++ CSharp/bitwise-and-of-numbers-range.cs
@@ -7,40 +7,22 @@ namespace BitWiseAndOfNumbersRange
 
     public class Solution
     {
-        //Math.Log2 was not allowed.
-        int MyLog2(int m)
-        {
-            var result = 0;
-            for (var i = 1; i < 32; i++)
-            {
-                var min = 1 << i;
-                if (min <= m)
-                {
-                    result = i;
-                }
-                else
-                {
-                    break;
-                }
-            }            
-            return result;
-        }
-
+        //Compare the MSB
         public int RangeBitwiseAnd(int m, int n)
         {
 
-            if (m == n)
-                return m & n;
+            var result = 0;
+            for (var i = 31; i >= 0; i--)
+            {
+                var mask = 1 << i;
+                var newM = m & mask;
+                if (newM == (n & mask))
+                    result |= newM;
+                else
+                    return result;
+            }
 
-            if (MyLog2(m) != MyLog2(n))
-                return 0;
-
-            var diff = MyLog2(n - m);
-
-            //If difference is 5, Log(2) is 2. Means all the last 2 digits would be changed if we iterate through m to n. 
-            //Thus the bit AND for last 2 bits must be 0. 
-            //So we can construct a mastk 11111111100 to filter out the result
-            return m & n & ~((1 << diff + 1) - 1);
+            return result;
         }
     }
 }
