0c79056231a5bee487e42da57054f431c26ce41b
Hainan Zhao <Hainan.Zhao@sage.com>
2021-01-24 16:38:55 +0800

    Added the kngiths move problem

--- CSharp/longest-palindromic-substring.cs
+++ CSharp/longest-palindromic-substring.cs
@@ -7,14 +7,30 @@ namespace LongestPalindromicSubstring
     public class Solution
     {
 
+        int GetIndex(int len, int i)
+        {
+            var sign = i % 2 == 1 ? 1 : -1;
+            if (len % 2 == 0)
+            {
+                return len / 2 - 1 + sign * ((i + 1) / 2);
+            }
+            else
+            {
+                return len / 2 + sign * ((i + 1) / 2);
+            }
+        }
         public string LongestPalindrome(string s)
         {
             var res = "";
             var max = 0;
 
-            for (var i = 0; i < s.Length - max / 2; i++)
+            for (var curr = 0; curr < s.Length; curr++)
             {
-                //Odd Loop
+                var i = GetIndex(s.Length, curr);
+                if (i < max / 2 - 1 || i > s.Length - max / 2)
+                    break;
+
+                //Odd length Loop
                 var l = i;
                 var r = i;
                 while (l >= 0 && r < s.Length && s[l] == s[r])
@@ -25,14 +41,13 @@ namespace LongestPalindromicSubstring
                         max = len;
                         res = s.Substring(l, len);
                     }
-
                     l--;
                     r++;
                 }
 
+                //Even length loop
                 l = i;
                 r = i + 1;
-
                 while (l >= 0 && r < s.Length && s[l] == s[r])
                 {
                     var len = r - l + 1;
@@ -41,14 +56,12 @@ namespace LongestPalindromicSubstring
                         max = len;
                         res = s.Substring(l, len);
                     }
-
                     l--;
                     r++;
                 }
-
             }
             return res;
-
         }
     }
+
 }
---
+++ CSharp/minimum-knight-moves.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinimumKnightsMoves
+{
+    class minimum_knight_moves
+    {
+        public class Solution
+        {
+            void Init(int[,] dict)
+            {
+                dict[0, 0] = 0;
+                dict[0, 1] = 3;
+                dict[0, 2] = 2;
+                dict[1, 0] = 3;
+                dict[1, 1] = 2;
+                dict[1, 2] = 1;
+                dict[2, 0] = 2;
+                dict[2, 1] = 1;
+                dict[2, 2] = 4;
+            }
+
+            int MinSteps(int[,] dict, int x, int y)
+            {
+                x = Math.Abs(x);
+                y = Math.Abs(y);
+
+                if (x == 0 && y == 0)
+                    return 0;
+
+                if (dict[x, y] != 0)
+                    return dict[x, y];
+                else
+                {
+                    dict[x, y] = Math.Min(MinSteps(dict, x - 1, y - 2) + 1, MinSteps(dict, x - 2, y - 1) + 1);
+                    return dict[x, y];
+                }
+            }
+
+            public int MinKnightMoves(int x, int y)
+            {
+                var dict = new int[Math.Max(3, Math.Abs(x) + 1), Math.Max(3, Math.Abs(y) + 1)];
+                Init(dict);
+
+                return MinSteps(dict, x, y);
+            }
+        }
+    }
+}
