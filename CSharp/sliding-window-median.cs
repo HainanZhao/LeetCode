f53621f36b0ecd9399f6ae5798ac0a4916af6ad4
Hainan Zhao <Hainan.Zhao@sage.com>
2021-01-31 22:01:16 +0800

    Updated Heap method for sliding window median

--- CSharp/sliding-window-median.cs
+++ CSharp/sliding-window-median.cs
@@ -7,51 +7,66 @@ namespace SlidingWindowMedian
 {
     public class Solution
     {
-        void Insert(List<int> list, int val)
+        void BalanceHeap(SortedSet<int> left, SortedSet<int> right, int[] nums)
         {
-
-            var index = list.BinarySearch(val);
-            if (index >= 0)
-                list.Insert(index, val);
+            if (Math.Abs(left.Count - right.Count) <= 1)
+                return;
+            else if (left.Count > right.Count)
+            {
+                var max = left.Max;
+                left.Remove(max);
+                right.Add(max);
+            }
             else
             {
-                index = ~index;
-                if (index >= list.Count)
-                    list.Add(val);
-                else
-                    list.Insert(index, val);
+                var min = right.Min;
+                right.Remove(min);
+                left.Add(min);
             }
         }
 
-        void Remove(List<int> list, int val)
+        double GetMedian(SortedSet<int> left, SortedSet<int> right, int[] nums, bool isOdd)
         {
-            var index = list.BinarySearch(val);
-            list.RemoveAt(index);
+            if (isOdd)
+                return left.Count > right.Count ? nums[left.Max] : nums[right.Min];
+            else
+                return nums[left.Max] * 0.5 + nums[right.Min] * 0.5;
         }
 
         public double[] MedianSlidingWindow(int[] nums, int k)
         {
-
-            var currList = nums[0..k].ToList();
-            currList.Sort();
+            var comparer = Comparer<int>.Create((x, y) => nums[x] != nums[y] ? (nums[x] > nums[y] ? 1 : -1) : (x - y));
+            var leftHeap = new SortedSet<int>(comparer);
+            var rightHeap = new SortedSet<int>(comparer);
             var isOdd = k % 2 == 1;
             var res = new double[nums.Length - k + 1];
-            for (var i = 0; i < nums.Length - k + 1; i++)
+
+            for (var i = 0; i < nums.Length; i++)
             {
-                if (isOdd)
-                    res[i] = currList[k / 2];
-                else
-                    res[i] = currList[k / 2] * 0.5 + currList[k / 2 - 1] * 0.5;
-
-
-                Remove(currList, nums[i]);
-                if (i < nums.Length - k)
+                var val = nums[i];
+                if (leftHeap.Count > 0 && nums[leftHeap.Max] >= val)
                 {
-                    Insert(currList, nums[i + k]);
+                    leftHeap.Add(i);
+                }
+                else if (rightHeap.Count > 0 && nums[rightHeap.Min] < val)
+                {
+                    rightHeap.Add(i);
+                }
+                else
+                {
+                    leftHeap.Add(i);
                 }
 
-            }
+                BalanceHeap(leftHeap, rightHeap, nums);
+                
+                if (i >= k - 1)
+                {
+                    res[i - k + 1] = GetMedian(leftHeap, rightHeap, nums, isOdd);
 
+                    if (!leftHeap.Remove(i - k + 1))
+                        rightHeap.Remove(i - k + 1);
+                }
+            }
             return res;
         }
     }
